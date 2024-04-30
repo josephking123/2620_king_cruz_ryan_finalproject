88c81a13029b387e1b24cb288459008db007bd9c
ryancl26 <143441008+ryancl26@users.noreply.github.com>
2024-04-30 18:08:15 -0400

    MainMenu fields

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,3 +1,20 @@
-public class MainMenu {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class MainMenu extends JFrame{
+
+    private JButton jButton1;
+    private JButton jButton2;
+    private JLabel jLabel1;
+
+    public MainMenu(){
+        initComponents();
+
+    }
+
+    public void run(){
+                new MainMenu().setVisible(true);
+    }
     
 }
