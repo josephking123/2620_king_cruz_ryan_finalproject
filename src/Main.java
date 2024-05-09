187733dfc109257656600d1060f0585f1701300a
ryancl26 <143441008+ryancl26@users.noreply.github.com>
2024-05-09 00:51:47 -0400

    more javadoc comments score and main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 import javax.swing.SwingUtilities;
 
+/**
+ * The Main class serves as the entry point for the Ping Pong Game application.
+ * It initializes the graphical user interface by creating and displaying the main menu.
+ */
 public class Main {
 
+    /**
+     * The main method, starting point of the application.
+     * It invokes the creation and display of the main menu on the Event Dispatch Thread.
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
--- src/Score.java
+++ src/Score.java
@@ -1,5 +1,9 @@
 import java.awt.*;
 
+/**
+ * The Score class represents the scoring mechanism for the game.
+ * It extends the Rectangle class and provides methods to draw the scores on the screen.
+ */
 public class Score extends Rectangle{
 
 	static int GAME_WIDTH;
@@ -7,10 +11,20 @@ public class Score extends Rectangle{
 	int player1;
 	int player2;
 	
+	/**
+     * Constructs a Score object with the specified game width and height.
+     * @param gameWidth The width of the game window.
+     * @param gameHeight The height of the game window.
+     */
 	Score(int GAME_WIDTH, int GAME_HEIGHT){
 		Score.GAME_WIDTH = GAME_WIDTH;
 		Score.GAME_HEIGHT = GAME_HEIGHT;
 	}
+
+	/**
+     * Draws the scores on the screen using the specified graphics context.
+     * @param g The Graphics object used for drawing.
+     */
 	public void draw(Graphics g) {
 		g.setColor(Color.white);
 		g.setFont(new Font("Consolas",Font.PLAIN,60));
